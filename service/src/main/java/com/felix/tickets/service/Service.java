df8a823d7d9855ae99489e01099f0fd707b34e87
felix1313 <vova.radyuk@gmail.com>
2016-05-16 21:51:32 +0300

    Implemented socket based webservice

--- service/src/main/java/com/felix/tickets/service/Service.java
+++ service/src/main/java/com/felix/tickets/service/Service.java
@@ -1,5 +1,39 @@
 package com.felix.tickets.service;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Service {
 
+	private Map<Integer,Client> clients = new HashMap<Integer,Client>();
+	
+
+	public Map<Integer, Client> getClients() {
+		return clients;
+	}
+
+	public void start() {
+		ServerSocket serverSocket;
+		try {
+			Integer port = 4774;
+			serverSocket = new ServerSocket(port);
+			while (true) {
+				Socket clientSocket = serverSocket.accept();
+				Client client = new Client(clientSocket, this);
+		//		log.info("starting new client thread " + client.getId());
+				client.start();
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void removeClient(Client client) {
+		this.clients.remove(client.getUser().getId());
+	}
 }
